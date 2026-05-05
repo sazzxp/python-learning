752bbcf68d4f5fd15ca612e52e5603a1fb269eda
sazzxp <shanoofkarat@gmail.com>
2026-05-05 13:32:18 +0300

    added grade finder code

--- conditions/grade_finder.py
+++ conditions/grade_finder.py
@@ -1,1 +1,10 @@
-
+print("Welcome to Grade Finder")
+mark = int(input("Write your mark? "))
+if mark >= 90 :
+    print("You passed your exam with full A's ")
+elif mark>= 75 :
+    print("You passed your exam with all B's ")
+elif mark >= 50 :
+    print("You passed your exam with all C's ")
+else :
+    print("You failed your exam")
